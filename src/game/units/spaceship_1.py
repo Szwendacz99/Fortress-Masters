62c221d1748d674106d19dea3536dceeabf09ea9
Lastyyy <simekm13@gmail.com>
2022-05-31 02:57:01 +0200

    Improved visibility of units by giving them glowing effect

--- src/game/units/spaceship_1.py
+++ src/game/units/spaceship_1.py
@@ -6,14 +6,14 @@ from game.rocket import Rocket
 
 
 class Spaceship_1(Unit):
-    path_blue = 'resources/img/Spaceship_pack_pack_01_BLUE.png'
-    path_red = 'resources/img/Spaceship_pack_pack_01_RED.png'
+    path_blue = 'resources/img/ship_blue1.png'
+    path_red = 'resources/img/ship_red1.png'
     path_blue_dead = 'resources/img/Spaceship_pack_pack_01_BLUE-dead.png'
     path_red_dead = 'resources/img/Spaceship_pack_pack_01_RED-dead.png'
 
     # Name of the class of the bullets that this unit will attack with
     bullet_type = Rocket
-    unit_size = 45
+    unit_size = 55
 
     def __init__(self, uuid: UUID, game, start_pos,
                  hp: int = 300, atk_damage: int = 30, atk_speed: int = 200, atk_range: int = 100, speed: float = 0.25,
--- src/game/units/spaceship_4.py
+++ src/game/units/spaceship_4.py
@@ -6,14 +6,14 @@ from game.rocket import Rocket
 
 
 class Spaceship_4(Unit):
-    path_blue = 'resources/img/Spaceship_pack_pack_04_BLUE.png'
-    path_red = 'resources/img/Spaceship_pack_pack_04_RED.png'
+    path_blue = 'resources/img/ship_blue4.png'
+    path_red = 'resources/img/ship_red4.png'
     path_blue_dead = 'resources/img/Spaceship_pack_pack_04_BLUE-dead.png'
     path_red_dead = 'resources/img/Spaceship_pack_pack_04_RED-dead.png'
 
     # Name of the class of the bullets that this unit will attack with
     bullet_type = Rocket
-    unit_size = 45
+    unit_size = 55
 
     def __init__(self, uuid: UUID, game, start_pos,
                  hp: int = 350, atk_damage: int = 40, atk_speed: int = 100, atk_range: int = 120, speed: float = 0.20,
